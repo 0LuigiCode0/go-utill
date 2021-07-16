32b9ef296f71f9fa9f32b56a2d4c878ddc212bf0
Шахтаров Вячеслав <ddd-dddu@yandex.ru>
2021-07-16 10:30:03 +0200

    #

--- gouitll.go
+++ gouitll.go
@@ -1,23 +1,23 @@
-// package goutill
+package goutill
 
-// import (
-// 	"bytes"
-// 	"io"
+import (
+	"bytes"
+	"io"
 
-// 	"github.com/0LuigiCode0/go-utill/form"
-// 	"github.com/0LuigiCode0/go-utill/helper"
-// 	"github.com/0LuigiCode0/go-utill/query"
-// 	"github.com/0LuigiCode0/go-utill/validator"
-// )
+	"github.com/0LuigiCode0/go-utill/form"
+	"github.com/0LuigiCode0/go-utill/helper"
+	"github.com/0LuigiCode0/go-utill/query"
+	"github.com/0LuigiCode0/go-utill/validator"
+)
 
-// //Генерирует mutlipart/form и content-type
-// func FormMarshal(in interface{}) (*bytes.Buffer, string, error) { return form.FormMarshal(in) }
+//Генерирует mutlipart/form и content-type
+func FormMarshal(in interface{}) (*bytes.Buffer, string, error) { return form.FormMarshal(in) }
 
-// //Генерирует строку query параметров
-// func QueryMarshal(in interface{}) (out string) { return query.QueryMarshal(in) }
+//Генерирует строку query параметров
+func QueryMarshal(in interface{}) (out string) { return query.QueryMarshal(in) }
 
-// //Validator виледирует данные
-// func Validator(isNull bool, data interface{}) error { return validator.Validator(isNull, data) }
+//Validator виледирует данные
+func Validator(isNull bool, data interface{}) error { return validator.Validator(isNull, data) }
 
-// //Парсит Json
-// func JsonParse(in io.Reader, out interface{}) (err error) { return helper.JsonParse(in, out) }
+//Парсит Json
+func JsonParse(in io.Reader, out interface{}) (err error) { return helper.JsonParse(in, out) }
